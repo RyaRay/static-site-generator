89a7b9096e68888779b2fd01aa46468c7d9ac323
RyaRay <raymond2.ryan@gmail.com>
2025-05-04 21:54:32 -0500

    split images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -31,3 +31,38 @@ def extract_markdown_links(text):
     pattern = r"\[(.*?)\]\((.*?)\)"
     matches = re.findall(pattern, text)
     return [(match[0], match[1]) for match in matches]
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        text = old_node.text
+        pattern = r"!\[(.*?)\]\((.*?)\)"
+        last_index = 0
+        for match in re.finditer(pattern, text):
+            start, end = match.span()
+            if start > last_index:
+                new_nodes.append(TextNode(text[last_index:start], TextType.TEXT))
+            alt_text, url = match.groups()
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+            last_index = end
+        if last_index < len(text):
+            new_nodes.append(TextNode(text[last_index:], TextType.TEXT))
+    return new_nodes
+    
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = extract_markdown_links(old_node.text)
+        for section in sections:
+            text, url = section
+            split_nodes.append(TextNode(text, TextType.LINK, url))
+        new_nodes.extend(split_nodes)
+    return new_nodes
