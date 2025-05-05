8a67b7e2cfefbedd4a103e5987fd42c5eee0e737
RyaRay <raymond2.ryan@gmail.com>
2025-05-04 21:43:10 -0500

    added inline markdown on text and images and links

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 from textnode import *
 from htmlnode import *
+from inline_markdown import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -38,6 +39,19 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_split_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
 
 if __name__ == "__main__":
     unittest.main()
