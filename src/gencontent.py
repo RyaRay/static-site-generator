4dad30bc338313f0e6c95db69c412a679e653fbf
RyaRay <raymond2.ryan@gmail.com>
2025-05-05 19:37:13 -0500

    deploying to internet

--- src/gencontent.py
+++ src/gencontent.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     # Create the destination directory if it doesn't exist
     os.makedirs(dest_dir_path, exist_ok=True)
     
@@ -20,7 +20,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 dest_file_path = os.path.join(dest_dir_path, html_file_name)
                 
                 # Generate the HTML page
-                generate_page(source_path, template_path, dest_file_path)
+                generate_page(source_path, template_path, dest_file_path, basepath)
         
         # If it's a directory, recurse into it
         else:
@@ -29,10 +29,10 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             new_dest_dir = os.path.join(dest_dir_path, item)
             
             # Recursively process the subdirectory
-            generate_pages_recursive(new_source_dir, template_path, new_dest_dir)
+            generate_pages_recursive(new_source_dir, template_path, new_dest_dir, basepath)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -46,14 +46,23 @@ def generate_page(from_path, template_path, dest_path):
     html = node.to_html()
 
     title = extract_title(markdown_content)
-    template = template.replace("{{ Title }}", title)
-    template = template.replace("{{ Content }}", html)
+    
+    # Replace content and title
+    complete_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
 
+    # Replace URLs
+    complete_html = complete_html.replace('href="/', f'href="{basepath}')
+    complete_html = complete_html.replace('src="/', f'src="{basepath}')
+
+    # Create directory if needed
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
+        
+    # Write to file
     to_file = open(dest_path, "w")
-    to_file.write(template)
+    to_file.write(complete_html)  # Use complete_html, not template
+    to_file.close()  # Don't forget to close the file
 
 
 def extract_title(md):
